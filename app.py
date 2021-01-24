8b35d05a74a1cd616d649cb60fb7073df1d558eb
Mir <23290678+geekmir@users.noreply.github.com>
2021-01-24 11:42:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import psycopg2
-
+import requests
 con = psycopg2.connect(database="post_db", user="post_user", password="password", host="127.0.0.1", port="5432")
 
+
+x = requests.get('psycopg2.connect(database="post_db", user="post_user", password="password", host="127.0.0.1", port="5432")')
+
+
+
+print(x.status_code)
 print("Database opened successfully")
 
 
