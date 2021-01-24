d4618f69964f7648887d4d055adbfc5014484a99
Mir <23290678+geekmir@users.noreply.github.com>
2021-01-24 10:36:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-con = psycopg2.connect(database="postgres", user="postgres", password="", host="127.0.0.1", port="5432")
+con = psycopg2.connect(database="post_db", user="post_user", password="", host="127.0.0.1", port="5432")
 
 print("Database opened successfully")
 
